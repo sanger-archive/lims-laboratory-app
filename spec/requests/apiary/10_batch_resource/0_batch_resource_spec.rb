323298ecbd361f7715d688554076ba7106069057
max <bmx007@gmail.com>
2013-05-06 16:29:21 +0100

    Spec generation. doesn't work

---
+++ spec/requests/apiary/10_batch_resource/0_batch_resource_spec.rb
@@ -0,0 +1,9 @@
+require "requests/apiary/10_batch_resource/spec_helper"
+describe "batch_resource", :batch => true do
+  include_context "use core context service"
+  it "batch_resource" do
+  # A batch groups order items together.
+
+
+  end
+end
---
+++ spec/requests/spec_helper.rb
@@ -0,0 +1,2 @@
+require 'spec_helper'
+require 'integrations/spec_helper'
