b7104ea78dd48b8227bbcb266016e7efb5bc502d
Loic <llh1@sanger.ac.uk>
2013-05-16 11:22:46 +0100

    Ability to add tubes into an existing tube rack

--- lib/lims-laboratory-app/laboratory/tube_rack/update_tube_rack.rb
+++ lib/lims-laboratory-app/laboratory/tube_rack/update_tube_rack.rb
@@ -12,8 +12,21 @@ module Lims::LaboratoryApp
         attribute :tube_rack, Laboratory::TubeRack, :required => true, :writer => :private
         attribute :aliquot_type, String, :required => false, :writer => :private
         attribute :aliquot_quantity, Numeric, :required => false, :writer => :private
+        # @attribute [Hash<String, Laboratory::Tube>] tubes
+        # @example
+        # {"A1" => tube_1, "B4" => tube_2}
+        attribute :tubes, Hash, :required => false, :writer => :private, :default => {}
 
+        # Add the new tubes first then update all the tubes if needed 
+        # with aliquot_type and aliquot_quantity.
+        # If the position of the tube rack is not empty,
+        # a RackPositionNotEmpty exception is raised.
+        # @see Laboratory::TubeRack#[]=
         def _call_in_session(session)
+          tubes.each do |location, tube|
+            tube_rack[position] = tube 
+          end
+
           tube_rack.each do |tube|
             if tube
               tube.each do |aliquot|
