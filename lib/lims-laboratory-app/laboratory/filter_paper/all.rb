975f39c4880d5f8e320d4e55ec295680c0770710
Karel <ke4@sanger.ac.uk>
2013-05-30 15:59:59 +0100

    [fix #50805157] Added CreateFilterPaper action

---
+++ lib/lims-laboratory-app/laboratory/filter_paper/all.rb
@@ -0,0 +1,1 @@
+require 'lims-laboratory-app/laboratory/filter_paper/create_filter_paper'
--- spec/models/laboratory/aliquot_shared.rb
+++ spec/models/laboratory/aliquot_shared.rb
@@ -1,4 +1,6 @@
-require 'lims-laboratory-app/laboratory/aliquot/all'
+require 'lims-laboratory-app/laboratory/sample'
+require 'lims-laboratory-app/laboratory/aliquot'
+
 module Lims::LaboratoryApp
   module Laboratory
     shared_context "aliquot factory" do
@@ -7,7 +9,6 @@ module Lims::LaboratoryApp
       end
 
       def new_aliquot(i=nil, j=nil, q=nil)
-        sample = Sample
         aliquot = Aliquot.new(:sample => new_sample(i,j))
         aliquot.quantity = q if q
         aliquot
@@ -15,3 +16,4 @@ module Lims::LaboratoryApp
     end
   end
 end
+
