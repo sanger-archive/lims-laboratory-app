b8e765f6a2123ddfa727345bd536c814f64b8ee2
Loic <llh1@sanger.ac.uk>
2013-06-11 11:47:41 +0100

    Sample filter for sample uuids array

--- lib/lims-laboratory-app/laboratory/sample/sample_sequel_filter.rb
+++ lib/lims-laboratory-app/laboratory/sample/sample_sequel_filter.rb
@@ -21,13 +21,18 @@ module Lims::Core
     module Sequel::Filters
       def sample_filter(criteria)
         resource_id_column = "#{self.table_name.to_s.chomp("s")}_id".to_sym
-        sample_uuid = @session.pack_uuid(criteria[:sample]["uuid"])
+        sample_uuids = criteria[:sample]["uuid"]
+        uuids = if sample_uuids.is_a?(Array)
+                  sample_uuids.map { |uuid| @session.pack_uuid(uuid) }
+                else
+                  @session.pack_uuid(sample_uuids)
+                end
         through = aliquot_to_resource_table(resource_id_column)
 
         aliquot_dataset = self.dataset.join(through, resource_id_column => :id).join(:aliquots, :id => :aliquot_id)
         sample_dataset = aliquot_dataset.join(:samples, :id => :sample_id).join(:uuid_resources, :key => :id).where(:uuid_resources__model_class => "sample")
 
-        self.class.new(self, sample_dataset.where(:uuid_resources__uuid => sample_uuid).qualify.distinct)
+        self.class.new(self, sample_dataset.where(:uuid_resources__uuid => uuids).qualify.distinct)
       end
 
       def aliquot_to_resource_table(resource_id_column)
