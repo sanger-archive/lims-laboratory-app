29a53263e9ee3240ee71608ec28b44acfb0624e7
max <bmx007@gmail.com>
2013-06-18 12:25:14 +0100

    Check Create Labellable error.

---
+++ spec/requests/errors/labels/create_labellable_spec.rb
@@ -0,0 +1,35 @@
+require "requests/errors/labels/spec_helper"
+describe "create_labellable" do
+  include_context "use core context service"
+  it "Add a label to an asset" do
+  # Don't pass a the labellable name
+    save_with_uuid Lims::LaboratoryApp::Laboratory::Tube.new => [1,2,3,4,0]
+
+    header('Accept', 'application/json')
+    header('Content-Type', 'application/json')
+
+    response = post "/labellables", <<-EOD
+    {
+    "labellable": {
+        "type": "resource",
+        "labels": {
+            "front barcode": {
+                "value": "1234-ABC",
+                "type": "sanger-barcode"
+            }
+        }
+    }
+}
+    EOD
+    response.should match_json_response(422, <<-EOD) 
+    {
+    "errors": {
+        "name": [
+            "Name must not be blank"
+        ]
+    }
+}
+    EOD
+
+  end
+end
