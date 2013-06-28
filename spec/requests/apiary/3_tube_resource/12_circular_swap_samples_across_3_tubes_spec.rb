4320cb3bc5ba76af7aa1aaf1e852e22e7397d8c4
Loic <llh1@sanger.ac.uk>
2013-06-28 14:39:27 +0100

    Add specs for swap samples

---
+++ spec/requests/apiary/3_tube_resource/12_circular_swap_samples_across_3_tubes_spec.rb
@@ -0,0 +1,178 @@
+require "requests/apiary/3_tube_resource/spec_helper"
+describe "circular_swap_samples_across_3_tubes", :tube => true do
+  include_context "use core context service"
+  it "circular_swap_samples_across_3_tubes" do
+    sample1 = Lims::LaboratoryApp::Laboratory::Sample.new(:name => 'sample 1')
+    sample2 = Lims::LaboratoryApp::Laboratory::Sample.new(:name => 'sample 2')
+    sample3 = Lims::LaboratoryApp::Laboratory::Sample.new(:name => 'sample 3')
+    
+    tube1 = Lims::LaboratoryApp::Laboratory::Tube.new 
+    tube1 << Lims::LaboratoryApp::Laboratory::Aliquot.new(:quantity => 10, :type => "DNA", :sample => sample1)
+    
+    tube2 = Lims::LaboratoryApp::Laboratory::Tube.new 
+    tube2 << Lims::LaboratoryApp::Laboratory::Aliquot.new(:quantity => 10, :type => "DNA", :sample => sample2)
+    
+    tube3 = Lims::LaboratoryApp::Laboratory::Tube.new 
+    tube3 << Lims::LaboratoryApp::Laboratory::Aliquot.new(:quantity => 10, :type => "DNA", :sample => sample3)
+    
+    save_with_uuid({
+      sample1 => [1,2,3,0,1], sample2 => [1,2,3,0,2], sample3 => [1,2,3,0,3],
+      tube1 => [1,2,3,1,1], tube2 => [1,2,3,1,2], tube3 => [1,2,3,1,3]
+    })
+
+    header('Accept', 'application/json')
+    header('Content-Type', 'application/json')
+
+    response = post "/actions/swap_samples", <<-EOD
+    {
+    "swap_samples": {
+        "swap_samples": [
+            {
+                "resource_uuid": "11111111-2222-3333-1111-111111111111",
+                "swaps": {
+                    "11111111-2222-3333-0000-111111111111": "11111111-2222-3333-0000-333333333333"
+                }
+            },
+            {
+                "resource_uuid": "11111111-2222-3333-1111-222222222222",
+                "swaps": {
+                    "11111111-2222-3333-0000-222222222222": "11111111-2222-3333-0000-111111111111"
+                }
+            },
+            {
+                "resource_uuid": "11111111-2222-3333-1111-333333333333",
+                "swaps": {
+                    "11111111-2222-3333-0000-333333333333": "11111111-2222-3333-0000-222222222222"
+                }
+            }
+        ]
+    }
+}
+    EOD
+    response.should match_json_response(200, <<-EOD) 
+    {
+    "swap_samples": {
+        "actions": {
+        },
+        "user": "user",
+        "application": "application",
+        "result": {
+            "resources": [
+                {
+                    "tube": {
+                        "actions": {
+                            "read": "http://example.org/11111111-2222-3333-1111-111111111111",
+                            "create": "http://example.org/11111111-2222-3333-1111-111111111111",
+                            "update": "http://example.org/11111111-2222-3333-1111-111111111111",
+                            "delete": "http://example.org/11111111-2222-3333-1111-111111111111"
+                        },
+                        "uuid": "11111111-2222-3333-1111-111111111111",
+                        "type": null,
+                        "max_volume": null,
+                        "aliquots": [
+                            {
+                                "sample": {
+                                    "actions": {
+                                        "read": "http://example.org/11111111-2222-3333-0000-333333333333",
+                                        "create": "http://example.org/11111111-2222-3333-0000-333333333333",
+                                        "update": "http://example.org/11111111-2222-3333-0000-333333333333",
+                                        "delete": "http://example.org/11111111-2222-3333-0000-333333333333"
+                                    },
+                                    "uuid": "11111111-2222-3333-0000-333333333333",
+                                    "name": "sample 3"
+                                },
+                                "quantity": 10,
+                                "type": "DNA",
+                                "unit": "mole"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "tube": {
+                        "actions": {
+                            "read": "http://example.org/11111111-2222-3333-1111-222222222222",
+                            "create": "http://example.org/11111111-2222-3333-1111-222222222222",
+                            "update": "http://example.org/11111111-2222-3333-1111-222222222222",
+                            "delete": "http://example.org/11111111-2222-3333-1111-222222222222"
+                        },
+                        "uuid": "11111111-2222-3333-1111-222222222222",
+                        "type": null,
+                        "max_volume": null,
+                        "aliquots": [
+                            {
+                                "sample": {
+                                    "actions": {
+                                        "read": "http://example.org/11111111-2222-3333-0000-111111111111",
+                                        "create": "http://example.org/11111111-2222-3333-0000-111111111111",
+                                        "update": "http://example.org/11111111-2222-3333-0000-111111111111",
+                                        "delete": "http://example.org/11111111-2222-3333-0000-111111111111"
+                                    },
+                                    "uuid": "11111111-2222-3333-0000-111111111111",
+                                    "name": "sample 1"
+                                },
+                                "quantity": 10,
+                                "type": "DNA",
+                                "unit": "mole"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "tube": {
+                        "actions": {
+                            "read": "http://example.org/11111111-2222-3333-1111-333333333333",
+                            "create": "http://example.org/11111111-2222-3333-1111-333333333333",
+                            "update": "http://example.org/11111111-2222-3333-1111-333333333333",
+                            "delete": "http://example.org/11111111-2222-3333-1111-333333333333"
+                        },
+                        "uuid": "11111111-2222-3333-1111-333333333333",
+                        "type": null,
+                        "max_volume": null,
+                        "aliquots": [
+                            {
+                                "sample": {
+                                    "actions": {
+                                        "read": "http://example.org/11111111-2222-3333-0000-222222222222",
+                                        "create": "http://example.org/11111111-2222-3333-0000-222222222222",
+                                        "update": "http://example.org/11111111-2222-3333-0000-222222222222",
+                                        "delete": "http://example.org/11111111-2222-3333-0000-222222222222"
+                                    },
+                                    "uuid": "11111111-2222-3333-0000-222222222222",
+                                    "name": "sample 2"
+                                },
+                                "quantity": 10,
+                                "type": "DNA",
+                                "unit": "mole"
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "swap_samples": [
+            {
+                "resource_uuid": "11111111-2222-3333-1111-111111111111",
+                "swaps": {
+                    "11111111-2222-3333-0000-111111111111": "11111111-2222-3333-0000-333333333333"
+                }
+            },
+            {
+                "resource_uuid": "11111111-2222-3333-1111-222222222222",
+                "swaps": {
+                    "11111111-2222-3333-0000-222222222222": "11111111-2222-3333-0000-111111111111"
+                }
+            },
+            {
+                "resource_uuid": "11111111-2222-3333-1111-333333333333",
+                "swaps": {
+                    "11111111-2222-3333-0000-333333333333": "11111111-2222-3333-0000-222222222222"
+                }
+            }
+        ]
+    }
+}
+    EOD
+
+  end
+end
