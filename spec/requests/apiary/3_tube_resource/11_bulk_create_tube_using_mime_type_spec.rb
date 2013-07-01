4d05a2948f479a66f79d866a74c40c56624cf45e
max <bmx007@gmail.com>
2013-07-01 09:55:45 +0100

    Add spec on tube :  generic create bulk

---
+++ spec/requests/apiary/3_tube_resource/11_bulk_create_tube_using_mime_type_spec.rb
@@ -0,0 +1,167 @@
+require "requests/apiary/3_tube_resource/spec_helper"
+describe "bulk_create_tube_using_mime_type", :tube => true do
+  include_context "use core context service"
+  it "bulk_create_tube_using_mime_type" do
+
+    header('Accept', 'application/json; representation=minimal')
+    header('Content-Type', 'application/json; representation=minimal')
+
+    response = post "/tubes", <<-EOD
+    {
+    "tubes": [
+        {
+            "type": "Eppendorf",
+            "max_volume": 2,
+            "aliquots": [
+                {
+                    "sample_uuid": "11111111-0000-0000-0000-111111111111",
+                    "type": "NA",
+                    "quantity": 5
+                }
+            ],
+            "labels": {
+                "front barcode": {
+                    "value": "1234-ABC",
+                    "type": "ean13-barcode"
+                },
+                "back barcode": {
+                    "value": "1234-ABC",
+                    "type": "sanger-barcode"
+                }
+            }
+        },
+        {
+            "type": "Eppendorf",
+            "max_volume": 10,
+            "aliquots": [
+                {
+                    "sample_uuid": "11111111-0000-0000-0000-222222222222",
+                    "type": "RNA",
+                    "quantity": 15
+                }
+            ]
+        },
+        {
+            "type": "New type",
+            "max_volume": 15,
+            "aliquots": [
+                {
+                    "sample_uuid": "11111111-0000-0000-0000-333333333333",
+                    "type": "DNA",
+                    "quantity": 25
+                }
+            ]
+        }
+    ]
+}
+    EOD
+    response.should match_json_response(200, <<-EOD) 
+    {
+    "tubes": [
+        {
+            "actions": {
+                "read": "http://example.org/11111111-2222-3333-4444-555555555555",
+                "create": "http://example.org/11111111-2222-3333-4444-555555555555",
+                "update": "http://example.org/11111111-2222-3333-4444-555555555555",
+                "delete": "http://example.org/11111111-2222-3333-4444-555555555555"
+            },
+            "uuid": "11111111-2222-3333-4444-555555555555",
+            "type": "Eppendorf",
+            "max_volume": 2,
+            "aliquots": [
+                {
+                    "sample": {
+                        "actions": {
+                            "read": "http://example.org/11111111-0000-0000-0000-111111111111",
+                            "create": "http://example.org/11111111-0000-0000-0000-111111111111",
+                            "update": "http://example.org/11111111-0000-0000-0000-111111111111",
+                            "delete": "http://example.org/11111111-0000-0000-0000-111111111111"
+                        },
+                        "uuid": "11111111-0000-0000-0000-111111111111",
+                        "name": "Sample 1"
+                    },
+                    "quantity": 5,
+                    "type": "NA",
+                    "unit": "mole"
+                }
+            ],
+            "labels": {
+                "actions": {
+                    "read": "http://example.org/11111111-2222-3333-4444-666666666666",
+                    "create": "http://example.org/11111111-2222-3333-4444-666666666666",
+                    "update": "http://example.org/11111111-2222-3333-4444-666666666666",
+                    "delete": "http://example.org/11111111-2222-3333-4444-666666666666"
+                },
+                "uuid": "11111111-2222-3333-4444-666666666666",
+                "front barcode": {
+                    "value": "1234-ABC",
+                    "type": "ean13-barcode"
+                },
+                "back barcode": {
+                    "value": "1234-ABC",
+                    "type": "sanger-barcode"
+                }
+            }
+        },
+        {
+            "actions": {
+                "read": "http://example.org/11111111-2222-3333-4444-777777777777",
+                "create": "http://example.org/11111111-2222-3333-4444-777777777777",
+                "update": "http://example.org/11111111-2222-3333-4444-777777777777",
+                "delete": "http://example.org/11111111-2222-3333-4444-777777777777"
+            },
+            "uuid": "11111111-2222-3333-4444-777777777777",
+            "type": "Eppendorf",
+            "max_volume": 10,
+            "aliquots": [
+                {
+                    "sample": {
+                        "actions": {
+                            "read": "http://example.org/11111111-0000-0000-0000-222222222222",
+                            "create": "http://example.org/11111111-0000-0000-0000-222222222222",
+                            "update": "http://example.org/11111111-0000-0000-0000-222222222222",
+                            "delete": "http://example.org/11111111-0000-0000-0000-222222222222"
+                        },
+                        "uuid": "11111111-0000-0000-0000-222222222222",
+                        "name": "Sample 1"
+                    },
+                    "quantity": 15,
+                    "type": "RNA",
+                    "unit": "mole"
+                }
+            ]
+        },
+        {
+            "actions": {
+                "read": "http://example.org/11111111-2222-3333-4444-888888888888",
+                "create": "http://example.org/11111111-2222-3333-4444-888888888888",
+                "update": "http://example.org/11111111-2222-3333-4444-888888888888",
+                "delete": "http://example.org/11111111-2222-3333-4444-888888888888"
+            },
+            "uuid": "11111111-2222-3333-4444-888888888888",
+            "type": "New type",
+            "max_volume": 15,
+            "aliquots": [
+                {
+                    "sample": {
+                        "actions": {
+                            "read": "http://example.org/11111111-0000-0000-0000-333333333333",
+                            "create": "http://example.org/11111111-0000-0000-0000-333333333333",
+                            "update": "http://example.org/11111111-0000-0000-0000-333333333333",
+                            "delete": "http://example.org/11111111-0000-0000-0000-333333333333"
+                        },
+                        "uuid": "11111111-0000-0000-0000-333333333333",
+                        "name": "Sample 1"
+                    },
+                    "quantity": 25,
+                    "type": "DNA",
+                    "unit": "mole"
+                }
+            ]
+        }
+    ]
+}
+    EOD
+
+  end
+end
