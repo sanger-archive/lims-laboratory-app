68f7ccbec7e2330f1d428b8ac3af8d5dd92603ab
Karel <ke4@sanger.ac.uk>
2013-10-08 13:48:27 +0100

    Fixed the specs

--- lib/lims-laboratory-app/laboratory/filter_paper/filter_paper_persistor.rb
+++ lib/lims-laboratory-app/laboratory/filter_paper/filter_paper_persistor.rb
@@ -7,7 +7,9 @@ module Lims::LaboratoryApp
     class FilterPaper
 
       # This trait is defining the persistor layer of the Filter Paper class.
-      does "lims/laboratory_app/container_persistor", :element => :location_aliquot, :table_name => :locations,
+      does "lims/laboratory_app/container_persistor",
+        :element => :location_aliquot,
+        :table_name => :locations,
         :contained_class => Aliquot
 
     end
--- spec/models/laboratory/filter_paper/bulk_create_filter_paper_spec.rb
+++ spec/models/laboratory/filter_paper/bulk_create_filter_paper_spec.rb
@@ -1,8 +1,10 @@
 require 'models/actions/spec_helper'
 require 'models/actions/action_examples'
+require 'models/laboratory/aliquot_shared'
 
 require 'lims-laboratory-app/laboratory/filter_paper/bulk_create_filter_paper'
 require 'lims-core/persistence/store'
+require 'lims-laboratory-app/laboratory/filter_paper/all'
 
 module Lims::LaboratoryApp
   module Laboratory
@@ -37,7 +39,7 @@ module Lims::LaboratoryApp
           context "empty filter papers" do
             let(:parameters) do
               [].tap do |filter_papers|
-                5.times do
+                number_of_filter_papers.times do
                   filter_papers << create_parameters
                 end
               end
@@ -49,9 +51,62 @@ module Lims::LaboratoryApp
               result = subject.call
               result.should be_a(Hash)
               result[:filter_papers].should be_a(Array)
-              result[:filter_papers].size.should == 5
-              # TODO ke4 add random filter paper creation
+              result[:filter_papers].size.should == number_of_filter_papers
+              result[:filter_papers].each do |filter_paper|
+                filter_paper.should be_a(FilterPaper)
+                filter_paper.number_of_rows.should == number_of_rows
+                filter_paper.number_of_columns.should == number_of_columns
+              end
             end
+          end
+
+          context "tubes with samples" do
+            include_context('aliquot factory')
+            let(:locations_description_value) do
+              {}.tap do |descriptions|
+                1.upto(number_of_rows) do |row|
+                  1.upto(number_of_columns) do |column|
+                    descriptions[FilterPaper.indexes_to_element_name(row-1, column-1)] = [
+                      {:sample => new_sample(row, column), :quantity => 1}
+                      ]
+                  end
+                end
+              end
+            end
+            let(:locations_description) { {
+              :locations_description => locations_description_value
+            } }
+            let(:parameters) do
+              create_parameters.merge!(locations_description)
+              [].tap do |filter_papers|
+                number_of_filter_papers.times do
+                  filter_papers << create_parameters
+                end
+              end
+            end
+
+          it_behaves_like "an action"
+           it "create 5 filter papers" do
+             Lims::Core::Persistence::Session.any_instance.should_receive(:save_all)
+             result = subject.call
+             result.should be_a(Hash)
+             result[:filter_papers].should be_a(Array)
+             result[:filter_papers].size.should == number_of_filter_papers
+             result[:filter_papers].each do |filter_paper|
+               filter_paper.should be_a(FilterPaper)
+               filter_paper.number_of_rows.should == number_of_rows
+               filter_paper.number_of_columns.should == number_of_columns
+               filter_paper.each_with_index do |location, i|
+                 k = 0
+                 location.each_with_index do |aliquot, j|
+                   aliquot[:sample].should == parameters[k][:locations_description][i][j][:sample]
+                   aliquot[:type].should == parameters[k][:locations_description][i][j][:type]
+                   aliquot[:quantity].should == parameters[k][:locations_description][i][j][:quantity]
+                 end
+                 k += 1
+               end
+             end
+           end
           end
 
         end
