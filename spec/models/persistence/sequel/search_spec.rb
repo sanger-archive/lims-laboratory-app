e582e570fdcd1c512efb399b70f0064ffd88c377
Karel <ke4@sanger.ac.uk>
2013-10-22 10:52:28 +0100

    simplified the parameters

--- spec/models/persistence/sequel/search_spec.rb
+++ spec/models/persistence/sequel/search_spec.rb
@@ -11,13 +11,13 @@ require 'lims-laboratory-app/laboratory/plate'
 module Lims::Core::Persistence
   describe "Search::CreateSearch" do
     def creating_a_search(description, model, filter)
-      Search.new( :description => description,
-                  :model => model, 
-                  :filter => filter).tap do |search|
-        store.with_session do |session|
-          session << search
-          uuid = session.uuid_for!(search)
-        end
+      store.with_session do |session|
+        search = Search.new( :description => description,
+                             :model => session.send(model).model,
+                             :filter => filter)
+        session << search
+        uuid = session.uuid_for!(search)
+        search
       end
     end
 
@@ -25,30 +25,31 @@ module Lims::Core::Persistence
     include_context "for application", "search test"
 
     let(:description)       { "search for a plate by ids" }
-    let(:model)             { Lims::LaboratoryApp::Laboratory::Plate }
-    let(:model_for_action)  { "plate" }
-    let(:criteria)          { { "id" => [1,2]} }
-    let(:criteria_for_act)  { { "id" => [1,2]} }
-    let(:filter)            { MultiCriteriaFilter.new(:criteria => criteria) }
-    let!(:existing_search) { creating_a_search(description, model, filter)}
+    let(:model)             { "plate" }
+    let(:condition)         { [1,2] }
+    let(:criteria_key)      { "id" }
+    let(:filter)            { MultiCriteriaFilter.new(:criteria => { criteria_key => condition}) }
+    let!(:existing_search)  { creating_a_search(description, model, filter)}
 
     subject do
       Search::CreateSearch.new(:store => store, :user => user, :application => application)  do |a,s|
         a.description = description
-        a.model       = model_for_action
-        a.criteria    = criteria_for_act
+        a.model       = model
+        a.criteria    = { criteria_key => condition }
       end
     end
 
     context "creating 2 or more similar search should not crash the DB" do
       it {
-        creating_a_search(description, model, filter).should be_a(Search)
-        result = subject.call
-        result.should be_a Hash
-        result[:search].tap do |returned_search|
-          returned_search.description.should == existing_search.description
-          returned_search.model.should == existing_search.model
-          returned_search.filter.should == existing_search.filter
+        store.with_session do |session|
+          creating_a_search(description, model, filter).should be_a(Search)
+          result = subject.call
+          result.should be_a Hash
+          result[:search].tap do |returned_search|
+            returned_search.description.should == existing_search.description
+            returned_search.model.should == existing_search.model
+            returned_search.filter.should == existing_search.filter
+          end
         end
       }
     end
