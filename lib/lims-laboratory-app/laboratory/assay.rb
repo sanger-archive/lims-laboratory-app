c32e19f826bc32e594e589da3310dded2232fa97
Karel <ke4@sanger.ac.uk>
2013-11-10 21:52:09 +0000

    added missing API doc comments for assay

--- lib/lims-laboratory-app/laboratory/assay.rb
+++ lib/lims-laboratory-app/laboratory/assay.rb
@@ -4,6 +4,16 @@ require 'lims-laboratory-app/laboratory/allele'
 
 module Lims::LaboratoryApp
   module Laboratory
+    # An Assay is a laboratory resource, which is an investigative (analytic)
+    # procedure in laboratory medicine, pharmacology, environmental biology,
+    # and molecular biology for qualitatively assessing or quantitatively
+    # measuring the presence or amount or the functional activity
+    # of a target entity (sample).
+    # An Assay can be put to well on a plate.
+    # An Assay must have:
+    # - a name (String)
+    # - an allele_x (A, C, G or T)
+    # - an allele_y (A, C, G or T)
     class Assay
       include Lims::Core::Resource
 
