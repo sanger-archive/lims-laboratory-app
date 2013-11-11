9b578d7c512a562426370139e22524c41dc6c2c4
Karel <ke4@sanger.ac.uk>
2013-11-11 20:54:30 +0000

    [#60334916] Added more spec for fluidigm labware

--- spec/models/laboratory/fluidigm_spec.rb
+++ spec/models/laboratory/fluidigm_spec.rb
@@ -16,23 +16,38 @@ module Lims::LaboratoryApp::Laboratory
   end
 
   describe Fluidigm do
+    let(:size)              { number_of_rows*number_of_columns }
+    let(:container) { Fluidigm::Well }
+    let(:error_container_does_not_exists) { Fluidigm::IndexOutOfRangeError }
+    subject { described_class.new(:number_of_columns => number_of_columns, :number_of_rows =>number_of_rows) }
+
     context "with 12x16 wells" do
       let(:number_of_rows)    { 16 }
       let(:number_of_columns) { 12 }
-      let(:size)              { number_of_rows*number_of_columns }
-
-      let(:container) { Fluidigm::Well }
-      let(:error_container_does_not_exists) { Fluidigm::IndexOutOfRangeError }
-
-      subject { described_class.new(:number_of_columns => number_of_columns, :number_of_rows =>number_of_rows) }
 
       its(:number_of_rows) {should == number_of_rows }
       its(:number_of_columns) { should == number_of_columns }
       its(:size) { should eq(size) }
 
-    it_behaves_like "a valid fluidigm"
-    it_behaves_like "a hash", :S1, :A4, :B14, :W34
-    it_behaves_like "labellable"
+      it_behaves_like "a valid fluidigm"
+      it_behaves_like "a hash", :S1, :A11, :S314, :W12
+      it_behaves_like "labellable"
     end
+
+    context "with 12x16 wells" do
+      let(:number_of_rows)    { 16 }
+      let(:number_of_columns) { 14 }
+
+      its(:number_of_rows) {should == number_of_rows }
+      its(:number_of_columns) { should == number_of_columns }
+      its(:size) { should eq(size) }
+
+      it_behaves_like "a valid fluidigm"
+      it_behaves_like "a hash", :S1, :A1, :S300, :Z12
+      it_behaves_like "a hash", :S10, :A2, :S400, :X12
+      it_behaves_like "a hash", :S100, :A8, :S500, :W12
+      it_behaves_like "labellable"
+    end
+
   end
 end
