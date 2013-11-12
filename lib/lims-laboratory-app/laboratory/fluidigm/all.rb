b23d22510721013d4bf4e614d6cee797803cc02c
Karel <ke4@sanger.ac.uk>
2013-11-12 09:36:53 +0000

    [#60335488] Added CreateFluidigm action

---
+++ lib/lims-laboratory-app/laboratory/fluidigm/all.rb
@@ -0,0 +1,3 @@
+require 'lims-laboratory-app/laboratory/fluidigm'
+require 'lims-laboratory-app/laboratory/fluidigm/fluidigm_persistor'
+require 'lims-laboratory-app/laboratory/fluidigm/create_fluidigm'
