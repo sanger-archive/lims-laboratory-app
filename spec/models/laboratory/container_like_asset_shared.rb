bc889989a28a3d5917378b932615b75af7d7fc4c
Karel <ke4@sanger.ac.uk>
2013-11-14 15:17:50 +0000

    [#60335558] Added spec for Fluidigm persistence layer

--- spec/models/laboratory/container_like_asset_shared.rb
+++ spec/models/laboratory/container_like_asset_shared.rb
@@ -13,22 +13,53 @@ module Lims::LaboratoryApp
     shared_context "container-like asset factory" do
       include_context "aliquot factory"
 
-      def new_plate_with_samples(sample_nb=5, quantity=nil)
-        new_container_with_samples(Plate, sample_nb, quantity)
+      def new_plate_with_samples(sample_nb=5, volume=nil)
+        new_container_with_samples(Plate, sample_nb, volume)
       end
 
-      def new_gel_with_samples(sample_nb=5, quantity=nil)
-        new_container_with_samples(Gel, sample_nb, quantity)
+      def new_gel_with_samples(sample_nb=5, volume=nil)
+        new_container_with_samples(Gel, sample_nb, volume)
+      end
+
+      def new_fluidigm_with_samples(sample_nb=5, volume=nil)
+        new_container_with_samples(Fluidigm, sample_nb, volume)
       end
 
       def new_container_with_samples(asset_to_create, sample_nb, volume=100, quantity=nil)
         asset_to_create.new(:number_of_rows => number_of_rows, :number_of_columns => number_of_columns).tap do |asset|
-          asset.each_with_index do |w, i|
-            1.upto(sample_nb) do |j|
-              w <<  new_aliquot(i,j,quantity)
-            end
-            w << Aliquot.new(:type => Aliquot::Solvent, :quantity => volume) if volume
+          unless asset_to_create == Fluidigm
+            populate_general_container_with_samples(asset, sample_nb, volume, quantity)
+          else
+            populate_fluidigm(asset, sample_nb, volume, quantity)
           end
+        end
+      end
+
+      def populate_general_container_with_samples(asset, sample_nb, volume, quantity)
+        asset.each_with_index do |w, i|
+          1.upto(sample_nb) do |j|
+            w <<  new_aliquot(i,j,quantity)
+          end
+          w << Aliquot.new(:type => Aliquot::Solvent, :quantity => volume) if volume
+        end
+      end
+
+      def populate_fluidigm(asset, sample_nb, volume, quantity)
+        size = number_of_rows*number_of_columns
+        # FLUIDIGM 96.96
+        if size == 192
+          0.upto(number_of_rows-1) do |row|
+            1.upto(number_of_columns/2) do |column|
+              index = row*number_of_columns/2 + column
+              1.upto(sample_nb) do |j|
+                asset["S#{index}"] << new_aliquot(index,j,quantity)
+                asset["A#{index}"] << new_snp_assay_aliquot(index,j,quantity)
+              end
+            end
+          end
+        # FLUIDIGM 192.24
+        elsif size == 224
+          # TODO
         end
       end
 
@@ -38,6 +69,10 @@ module Lims::LaboratoryApp
 
       def new_empty_gel
         new_empty_container(Gel)
+      end
+
+      def new_empty_fluidigm
+        new_empty_container(Fluidigm)
       end
 
       def new_empty_container(asset_to_create)
