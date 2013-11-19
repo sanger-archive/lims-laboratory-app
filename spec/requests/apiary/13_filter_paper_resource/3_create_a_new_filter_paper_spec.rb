d878958015ca6fef65c1a3564bcaa8add79a0762
Loic <llh1@sanger.ac.uk>
2013-11-19 10:48:05 +0000

    Fix specs

--- spec/requests/apiary/13_filter_paper_resource/3_create_a_new_filter_paper_spec.rb
+++ spec/requests/apiary/13_filter_paper_resource/3_create_a_new_filter_paper_spec.rb
@@ -3,10 +3,6 @@ describe "create_a_new_filter_paper", :filter_paper => true do
   include_context "use core context service"
   it "create_a_new_filter_paper" do
   # **Create a new filter paper with a sample.**
-  # 
-  # * `number_of_rows` number of rows of the filter paper
-  # * `number_of_columns` number of columns of the filter paper
-  # * `locations_description` map aliquots to locations
     save_with_uuid Lims::LaboratoryApp::Laboratory::Sample.new(:name => 'sample 1') => [1,2,3,4,6]
 
     header('Content-Type', 'application/json')
@@ -15,17 +11,13 @@ describe "create_a_new_filter_paper", :filter_paper => true do
     response = post "/filter_papers", <<-EOD
     {
     "filter_paper": {
-        "number_of_rows": 2,
-        "number_of_columns": 2,
-        "locations_description": {
-            "A1": [
-                {
-                    "sample": "11111111-2222-3333-4444-666666666666",
-                    "type": "DNA",
-                    "quantity": 2
-                }
-            ]
-        }
+        "aliquots": [
+            {
+                "sample_uuid": "11111111-2222-3333-4444-666666666666",
+                "type": "DNA",
+                "quantity": 2
+            }
+        ]
     }
 }
     EOD
@@ -39,36 +31,23 @@ describe "create_a_new_filter_paper", :filter_paper => true do
             "create": "http://example.org/11111111-2222-3333-4444-555555555555"
         },
         "uuid": "11111111-2222-3333-4444-555555555555",
-        "number_of_rows": 2,
-        "number_of_columns": 2,
-        "locations": {
-            "A1": [
-                {
-                    "sample": {
-                        "actions": {
-                            "read": "http://example.org/11111111-2222-3333-4444-666666666666",
-                            "update": "http://example.org/11111111-2222-3333-4444-666666666666",
-                            "delete": "http://example.org/11111111-2222-3333-4444-666666666666",
-                            "create": "http://example.org/11111111-2222-3333-4444-666666666666"
-                        },
-                        "uuid": "11111111-2222-3333-4444-666666666666",
-                        "name": "sample 1"
+        "aliquots": [
+            {
+                "sample": {
+                    "actions": {
+                        "read": "http://example.org/11111111-2222-3333-4444-666666666666",
+                        "update": "http://example.org/11111111-2222-3333-4444-666666666666",
+                        "delete": "http://example.org/11111111-2222-3333-4444-666666666666",
+                        "create": "http://example.org/11111111-2222-3333-4444-666666666666"
                     },
-                    "quantity": 2,
-                    "type": "DNA",
-                    "unit": "mole"
-                }
-            ],
-            "A2": [
-
-            ],
-            "B1": [
-
-            ],
-            "B2": [
-
-            ]
-        }
+                    "uuid": "11111111-2222-3333-4444-666666666666",
+                    "name": "sample 1"
+                },
+                "quantity": 2,
+                "type": "DNA",
+                "unit": "mole"
+            }
+        ]
     }
 }
     EOD
