7ef08420b7b2f3a9d867f102b03aa2edbb618dec
Loic <llh1@sanger.ac.uk>
2013-11-19 12:00:20 +0000

    Refactor create actions to use the create receptacle action trait

--- lib/lims-laboratory-app/laboratory/filter_paper/create_filter_paper.rb
+++ lib/lims-laboratory-app/laboratory/filter_paper/create_filter_paper.rb
@@ -1,46 +1,16 @@
 require 'lims-laboratory-app/laboratory/filter_paper'
-require 'lims-laboratory-app/laboratory/create_labellable_resource_action'
-require 'lims-laboratory-app/laboratory/sample/create_sample_shared'
+require 'lims-laboratory-app/laboratory/create_receptacle_action_trait'
 
 module Lims::LaboratoryApp
   module Laboratory
     class FilterPaper
       class CreateFilterPaper
-        include CreateLabellableResourceAction
-        include Laboratory::Sample::CreateSampleShared
 
-        attribute :aliquots, Array, :default => []
+        does "lims/laboratory_app/laboratory/receptacle/create_receptacle_action", {
+          :receptacle_name => "filter_paper",
+          :receptacle_class => Laboratory::FilterPaper
+        }
 
-        def initialize(*args, &block)
-          @name = "Create Filter Paper"
-          super(*args, &block)
-        end
-
-        # TODO : to refactor with create_tube
-        def create(session)
-          filter_paper = Laboratory::FilterPaper.new
-          session << filter_paper
-          count = 1
-          if aliquots
-            aliquots.each do |aliquot|
-              # The sample uuid comes from lims-management-app, 
-              # as a result, the sample is not present in the 
-              # lims-laboratory-app sample table. The following 
-              # creates a new sample with the expected uuid.
-              aliquot_ready = aliquot.mash do |k,v|
-                case k.to_s
-                when "sample_uuid" then 
-                  count += 1
-                  ["sample", create_sample(session, "Sample #{count}", v)] 
-                else 
-                  [k,v]
-                end
-              end
-              filter_paper << Laboratory::Aliquot.new(aliquot_ready)
-            end
-          end
-          {:filter_paper => filter_paper, :uuid => session.uuid_for!(filter_paper)}
-        end
       end
 
       Create = CreateFilterPaper
--- lib/lims-laboratory-app/laboratory/tube/create_tube.rb
+++ lib/lims-laboratory-app/laboratory/tube/create_tube.rb
@@ -1,48 +1,21 @@
 # vi: ts=2:sts=2:et:sw=2  spell:spelllang=en  
 require 'lims-laboratory-app/laboratory/tube'
-require 'lims-laboratory-app/laboratory/create_labellable_resource_action'
-require 'lims-laboratory-app/laboratory/sample/create_sample_shared'
+require 'lims-laboratory-app/laboratory/create_receptacle_action_trait'
 
 module Lims::LaboratoryApp
   module Laboratory
     class Tube
       class CreateTube
-        include CreateLabellableResourceAction
-        include Laboratory::Sample::CreateSampleShared
 
-        attribute :aliquots, Array, :default => []
+        does "lims/laboratory_app/laboratory/receptacle/create_receptacle_action", {
+          :receptacle_name => "tube",
+          :receptacle_class => Laboratory::Tube,
+          :extra_parameters => [:type, :max_volume]
+        }
+
         attribute :type, String, :required => false
         attribute :max_volume, Numeric, :required => false
 
-        def initialize(*args, &block)
-          @name = "Create Tube"
-          super(*args, &block)
-        end
-
-        def create(session)
-          tube = Laboratory::Tube.new(:type => type, :max_volume => max_volume)
-          session << tube
-          count = 1
-          if aliquots
-            aliquots.each do |aliquot|
-              # The sample uuid comes from lims-management-app, 
-              # as a result, the sample is not present in the 
-              # lims-laboratory-app sample table. The following 
-              # creates a new sample with the expected uuid.
-              aliquot_ready = aliquot.mash do |k,v|
-                case k.to_s
-                when "sample_uuid" then 
-                  count += 1
-                  ["sample", create_sample(session, "Sample #{count}", v)] 
-                else 
-                  [k,v]
-                end
-              end
-              tube << Laboratory::Aliquot.new(aliquot_ready)
-            end
-          end
-          {:tube => tube, :uuid => session.uuid_for!(tube)}
-        end
       end
     end
   end
