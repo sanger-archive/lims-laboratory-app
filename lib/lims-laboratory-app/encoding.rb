e6305bf4e55e5909db18d0d576e21c31462c70a7
Loic <llh1@sanger.ac.uk>
2013-11-22 17:00:48 +0000

    Refactor encoding to update properly the api encoder

--- lib/lims-laboratory-app/encoding.rb
+++ lib/lims-laboratory-app/encoding.rb
@@ -1,32 +1,37 @@
 require 'lims-api/core_resource'
 
 module Lims::Api
-  module CoreResource::Encoder
-    module Representation
-
-      module Default
-        def self.included(klass)
-          klass.class_eval do
-            alias :original_to_hash_stream :to_hash_stream
+  class CoreResource
+    module Encoder
+      module Representation
+        module Default
+          def self.included(klass)
+            klass.class_eval do
+              alias :original_to_hash_stream :to_hash_stream
+            end
           end
         end
-      end
 
-      module Labellable
-        def to_hash_stream(h)
-          Default::original_to_hash_stream(h)
-          object.labellable_to_stream(h, @mime_type)
+        module Labellable
+          def to_hash_stream(h)
+            original_to_hash_stream(h)
+            object.labellable_to_stream(h, @mime_type)
+          end
         end
-      end
 
-      module NoLabellable
-        def to_hash_stream(h)
-          Default::original_to_hash_stream(h)
+        module NoLabellable
+          def to_hash_stream(h)
+            original_to_hash_stream(h)
+          end
         end
       end
     end
 
-    include Representation::Default
-    include Representation::Labellable
+    Encoders.find { |e| e == JsonEncoder }.tap do |json_encoder|
+      json_encoder.class_eval do
+        include Encoder::Representation::Default
+        include Encoder::Representation::Labellable
+      end
+    end
   end
 end
