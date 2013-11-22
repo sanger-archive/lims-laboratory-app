d4a06c3f90ebbee5f3caf36ff9ea1e7bab4e375b
Loic <llh1@sanger.ac.uk>
2013-11-22 16:16:36 +0000

    LabelledResource displays the labellable

--- lib/lims-laboratory-app/labels/labellable/labelled_resource.rb
+++ lib/lims-laboratory-app/labels/labellable/labelled_resource.rb
@@ -3,10 +3,18 @@ module Lims::LaboratoryApp
     class Labellable
       module LabelledResource
         def labellable_to_stream(s, mime_type)
-          if object.labellable
+          labellable = object.labellable if object.respond_to?(:labellable)
+
+          if labellable.nil? && @context.last_session
+            @context.last_session.tap do |session|
+              labellable = session.labellable[{:name => uuid, :type => "resource"}]
+            end
+          end
+
+          if labellable
             s.add_key "labels"
             s.with_hash do
-              resource = @context.resource_for(object.labellable, @context.find_model_name(object.labellable.class))
+              resource = @context.resource_for(labellable, @context.find_model_name(labellable.class))
               resource.encoder_for([mime_type]).actions_to_stream(s)
               s.add_key "uuid"
               s.add_value resource.uuid
