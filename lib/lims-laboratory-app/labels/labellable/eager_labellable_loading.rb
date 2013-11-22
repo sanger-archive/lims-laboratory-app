065cfc4940b67c6d36ce3aa307d6d063cc834dde
Loic <llh1@sanger.ac.uk>
2013-11-22 16:16:08 +0000

    EagerLabellableLoading load the labellable and bind them to a resource

--- lib/lims-laboratory-app/labels/labellable/eager_labellable_loading.rb
+++ lib/lims-laboratory-app/labels/labellable/eager_labellable_loading.rb
@@ -1,26 +1,35 @@
+require 'lims-core/persistence/persistor_module'
+
 module Lims::Core
   module Persistence
     module PersistorModule
       module EagerLabellableLoading
-        include BasePersistorModule
 
         def self.defined_for?(model)
           ["tube"].include?(model)
         end
 
-        def call_persistor_module(states)
-          super(states)
+        def eager_labellable_loading(states, *params)
+          load_labellables(states).each do |labellable|
+            labelled_resource_state = states.find { |state| state.uuid_resource.uuid == labellable.name }
+            next unless labelled_resource_state
+            labelled_resource = labelled_resource_state.resource
+            bind(labelled_resource, labellable)
+          end
+        end
+
+        def load_labellables(states)
           uuid_resources = states.map { |state| @session.uuid_resource_for(state) }
           uuids = uuid_resources.map(&:uuid).map { |uuid| @session.pack_uuid(uuid) }
           @session.labellable.find_by(:name => uuids, :type => "resource")
         end
 
         def bind(resource, labellable)
-          resource.extend Labellable
+          resource.extend LabelledResource
           resource.labellable = labellable
         end
 
-        module Labellable
+        module LabelledResource
           def self.extended(k)
             k.instance_eval do
               class << self
