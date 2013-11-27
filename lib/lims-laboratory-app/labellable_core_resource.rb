8f68f6edb3a16ac2bfb040c2d35f1e09559019e7
Loic <llh1@sanger.ac.uk>
2013-11-27 14:05:46 +0000

    Display the labellable from the resource attribute if it exists, otherwise load the labellable

--- lib/lims-laboratory-app/labellable_core_resource.rb
+++ lib/lims-laboratory-app/labellable_core_resource.rb
@@ -3,6 +3,27 @@ require 'lims-api/core_resource'
 module Lims::LaboratoryApp
   class LabellableCoreResource < Lims::Api::CoreResource
 
+    def labellable_to_stream(s, mime_type)
+      labellable = object.labellable if object.is_a? Lims::LaboratoryApp::WithLabellable
+
+      if labellable.nil? && @context.last_session
+        @context.last_session.tap do |session|
+          labellable = session.labellable[{:name => uuid, :type => "resource"}]
+        end
+      end
+
+      if labellable
+        s.add_key "labels"
+        s.with_hash do
+          resource = @context.resource_for(labellable, @context.find_model_name(labellable.class))
+          resource.encoder_for([mime_type]).actions_to_stream(s)
+          s.add_key "uuid"
+          s.add_value resource.uuid
+          resource.labels_to_stream(s, mime_type)
+        end
+      end
+    end
+
     module Encoder
       # TODO: when using ruby 2.0, the following code can be replaced
       # using "prepend" as it includes the module below the class in 
