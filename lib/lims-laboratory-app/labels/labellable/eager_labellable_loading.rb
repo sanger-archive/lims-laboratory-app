e74221820c9f4ade1c5d6cf151448e6c279bf2f1
Loic <llh1@sanger.ac.uk>
2013-11-29 12:50:10 +0000

    Refactor eager labellable loading (included by laboratory persistor instead of being extended from the core)

--- lib/lims-laboratory-app/labels/labellable/eager_labellable_loading.rb
+++ lib/lims-laboratory-app/labels/labellable/eager_labellable_loading.rb
@@ -1,30 +1,12 @@
-module Lims
-  module Core::Persistence
-    module PersistorModule
+module Lims::LaboratoryApp
+  module Labels
+    class Labellable
       module EagerLabellableLoading
-
-        # @param [Lims::Core::Persistence::Persistor] persistor
-        # @return [Bool]
-        def self.defined_for?(persistor)
-          labellable_resource_model_names.include?(persistor.model)
-        end
-
-        # @return [Array]
-        # We select the resource which are under the laboratory 
-        # namespace and which are Lims::Core::Resource.
-        def self.labellable_resource_model_names
-          @labellable_resource_model_names ||= Lims::LaboratoryApp::Laboratory.constants.inject([]) do |m,c|
-            model_class = Lims::LaboratoryApp::Laboratory.const_get(c)
-            m << model_class if model_class.ancestors.include?(Lims::Core::Resource)
-            m
-          end
-        end
-
         # @param [Lims::Core::Persistence::Persistor] persistor
         # The method load_children of the persistor instance is 
         # overriden to call eager_load_labellables.
-        def self.extended(persistor)
-          persistor.instance_eval do
+        def self.included(klass)
+          klass.class_eval do
             alias :load_children_old :load_children
             def load_children(states, *params)
               eager_load_labellables(states, *params)
@@ -61,14 +43,11 @@ module Lims
     end
   end
 
-
-  module LaboratoryApp
-    module WithLabellable
-      def self.extended(k)
-        k.instance_eval do
-          class << self
-            attr_accessor :labellable
-          end
+  module WithLabellable
+    def self.extended(k)
+      k.instance_eval do
+        class << self
+          attr_accessor :labellable
         end
       end
     end
