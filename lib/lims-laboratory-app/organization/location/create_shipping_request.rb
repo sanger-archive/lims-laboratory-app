41b8f38f1dbaded55cd30a44546d4432e04973cd
Karel <ke4@sanger.ac.uk>
2014-01-03 11:00:32 +0000

    [#63137240] Added create action for ShippingRequest

---
+++ lib/lims-laboratory-app/organization/location/create_shipping_request.rb
@@ -0,0 +1,29 @@
+require 'lims-core/actions/action'
+require 'lims-laboratory-app/organization/location/shipping_request'
+
+module Lims::LaboratoryApp
+  module Organization
+    class Location
+
+      class CreateShippingRequest
+        include Lims::Core::Actions::Action
+
+        attribute :name, String, :required => true
+        attribute :location, Location, :required => true
+
+        def _call_in_session(session)
+          shipping_request = ShippingRequest.new( {
+              :name     => name,
+              :location  => location
+          })
+          session << shipping_request
+
+          { :shipping_request => shipping_request, :uuid => session.uuid_for!(shipping_request) }
+        end
+      end
+
+      Create = CreateShippingRequest
+
+    end
+  end
+end
