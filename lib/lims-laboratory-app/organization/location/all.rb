a4bf58e2163fef8dd53a7bf620c3b7d68ad8eb35
Karel <ke4@sanger.ac.uk>
2014-01-07 12:58:58 +0000

    [#63365338]added location filter

--- lib/lims-laboratory-app/organization/location/all.rb
+++ lib/lims-laboratory-app/organization/location/all.rb
@@ -1,5 +1,7 @@
 require('lims-laboratory-app/organization/location/location_persistor')
 require('lims-laboratory-app/organization/location/create_location')
+require('lims-laboratory-app/organization/location/location_filter')
+require('lims-laboratory-app/organization/location/location_sequel_filter')
 require('lims-laboratory-app/organization/location/shipping_request')
 require('lims-laboratory-app/organization/location/shipping_request_persistor')
 require('lims-laboratory-app/organization/location/shipping_request_resource')
