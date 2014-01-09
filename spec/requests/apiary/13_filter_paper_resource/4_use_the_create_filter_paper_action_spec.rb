617681bd89ef7a5c6da53c4be70bbba6a5a844f1
Karel <ke4@sanger.ac.uk>
2014-01-09 17:39:24 +0000

    fix filter_paper apiary specs

--- spec/requests/apiary/13_filter_paper_resource/4_use_the_create_filter_paper_action_spec.rb
+++ spec/requests/apiary/13_filter_paper_resource/4_use_the_create_filter_paper_action_spec.rb
@@ -58,6 +58,7 @@ describe "use_the_create_filter_paper_action", :filter_paper => true do
             "uuid": "11111111-2222-3333-4444-555555555555"
         },
         "labels": null,
+        "location": null,
         "aliquots": [
             {
                 "sample_uuid": "11111111-2222-3333-4444-666666666666",
