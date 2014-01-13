6a6d9cc674ad33072d518315fa7156591f29e0da
Karel <ke4@sanger.ac.uk>
2014-01-13 14:10:12 +0000

    [#63457476] added location to tube rack's specs

--- spec/models/laboratory/tube_rack/tube_rack_move_spec.rb
+++ spec/models/laboratory/tube_rack/tube_rack_move_spec.rb
@@ -2,6 +2,7 @@
 require 'models/actions/action_examples'
 require 'models/persistence/sequel/store_shared'
 require 'models/laboratory/tube_rack_shared'
+require 'models/laboratory/location_shared'
 
 # Model requirements
 require 'lims-laboratory-app/laboratory/tube_rack/tube_rack_move'
@@ -14,6 +15,7 @@ module Lims::LaboratoryApp
         include_context "for application", "test tube rack move"
         include_context "sequel store"
         include_context "tube_rack factory"
+        include_context "define location"
 
         let(:number_of_rows) { 8 }
         let(:number_of_columns) { 12 }
