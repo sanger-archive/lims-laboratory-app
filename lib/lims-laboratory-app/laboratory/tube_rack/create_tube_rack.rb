93e7c7eb1fdee404563cc1d5952ceb1d5d3fab61
Karel <ke4@sanger.ac.uk>
2014-01-13 14:11:58 +0000

    [#63457476] added location to tube rack

--- lib/lims-laboratory-app/laboratory/tube_rack/create_tube_rack.rb
+++ lib/lims-laboratory-app/laboratory/tube_rack/create_tube_rack.rb
@@ -1,6 +1,7 @@
 # vi: ts=2:sts=2:et:sw=2  spell:spelllang=en  
 require 'lims-core/actions/action'
 require 'lims-laboratory-app/laboratory/tube_rack'
+require 'lims-laboratory-app/organization/location'
 
 module Lims::LaboratoryApp
   module Laboratory
@@ -12,13 +13,19 @@ module Lims::LaboratoryApp
           attribute :"number_of_#{w}s", Fixnum, :required => true, :gte => 0, :writer => :private
         end
 
+        attribute :location, Lims::LaboratoryApp::Organization::Location, :default => nil
+
         # @attribute [Hash<String, Laboratory::Tube>] tubes description
         # @example
         #     {"A1" => tube_1, "B4" => tube_2}
         attribute :tubes, Hash, :default => {}
 
         def _call_in_session(session)
-          tube_rack = Laboratory::TubeRack.new(:number_of_columns => number_of_columns, :number_of_rows => number_of_rows)
+          tube_rack = Laboratory::TubeRack.new(
+            :number_of_columns => number_of_columns,
+            :number_of_rows => number_of_rows,
+            :location => location
+          )
           session << tube_rack
 
           tubes.each do |position, tube|
