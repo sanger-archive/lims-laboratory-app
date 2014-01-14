fe1979b50016df1e0b7806b9d2036037769975f1
Karel <ke4@sanger.ac.uk>
2014-01-14 10:37:48 +0000

    [#63457476] added UpdateLocation action

--- lib/lims-laboratory-app/organization/location/all.rb
+++ lib/lims-laboratory-app/organization/location/all.rb
@@ -1,3 +1,4 @@
 require('lims-laboratory-app/organization/location/location_persistor')
 require('lims-laboratory-app/organization/location/create_location')
+require('lims-laboratory-app/organization/location/update_location')
 require('lims-laboratory-app/organization/location')
